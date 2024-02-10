e233c7b439def86d715b217ecd3ca4b2d9683197
Nathen Wiley <nzw0037@auburn.edu>
2024-02-10 08:22:00 -0800

    Initial Commit

---
+++ MyAdventure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAdventure
+//
+//  Created by user250623 on 2/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
