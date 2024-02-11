c18de61a102a8fb377fc5a46150146fe7b245fcf
Nathen Wiley <nzw0037@auburn.edu>
2024-02-11 13:31:38 -0800

    Completed program

--- MyAdventure/StoryContent.swift
+++ MyAdventure/StoryContent.swift
@@ -8,69 +8,40 @@
 import Foundation
 let unknownDestination = -1
 
-struct StoryBrain {
+struct StoryContent {
     
     let stories = [
         Story(
-            title: "You're walking home at night when a sudden downpour begins. Seeking shelter, you stumble upon an old, unmarked train station. The next train is headed to an unknown destination.",
-            choice1: "Board the train. Adventure awaits!", choice1Destination: 2,
-            choice2: "Wait out the storm. It's safer here.", choice2Destination: 1
+            title: "Lost in the Forest: As the sun sets, you realize you've strayed far from the hiking trail and are now hopelessly lost. With night approaching, you spot a dim light through the trees.",
+            choice1: "Head towards the light. It could be help.", choice1Destination: 2,
+            choice2: "Stay put. Someone will come looking.", choice2Destination: 1
         ),
         Story(
-            title: "The storm intensifies, but the station starts to feel eerie. You notice the schedule hasn’t been updated in years.",
-            choice1: "Maybe I should take my chances on the train...", choice1Destination: 2,
-            choice2: "No, I must stay. The storm will pass.", choice2Destination: 3
+            title: "Staying put seems safe, but as hours pass, the temperature drops. You're not prepared to spend the night.",
+            choice1: "Try to make a fire with what you have.", choice1Destination: 3,
+            choice2: "Keep waiting. Someone has to notice you're gone.", choice2Destination: 4
         ),
         Story(
-            title: "The train is oddly luxurious and quiet. You meet the conductor, who offers you two tickets; one leads to 'Yesterday', the other to 'Tomorrow'.",
-            choice1: "Take me to 'Yesterday'. I long for the past.", choice1Destination: 4,
-            choice2: "I choose 'Tomorrow'. It's time to move forward.", choice2Destination: 5
+            title: "As you approach the light, you find a small cabin. An old ranger answers your knock, offering shelter and a phone to call for help.",
+            choice1: "Thank them and call for a ride home.", choice1Destination: 5,
+            choice2: "Politely decline, fearing they might be dangerous.", choice2Destination: 4
         ),
         Story(
-            title: "The station becomes cold and the lights flicker. You’re not alone; whispers tell tales of a ghost train. The air grows colder as the night deepens.",
-            choice1: "I must confront the whispers.", choice1Destination: 6,
-            choice2: "I should never have left home...", choice2Destination: 0
+            title: "Your attempt at making a fire fails. The cold becomes unbearable, and you start to shiver uncontrollably.",
+            choice1: "Head towards the light in search of help", choice1Destination: 2,
+            choice2: "Stay where you are and hope someone finds you.", choice2Destination: 4
         ),
         Story(
-            title: "You arrive at 'Yesterday', finding yourself in your old hometown. It's the day before a regretful event. You have a chance to make things right.",
-            choice1: "Prevent the regretful event.", choice1Destination: 7,
-            choice2: "Observe, but don't interfere. The past is delicate.", choice2Destination: 8
+            title: "The night passes slowly and no one comes. By morning, you're cold, hungry, and still lost.",
+            choice1: "End", choice1Destination: 0,
+            choice2: "End", choice2Destination: 0
         ),
         Story(
-            title: "Tomorrow brings you to a utopia, but you're offered a job: maintain the utopia by doing tasks that challenge your morals.",
-            choice1: "Accept. Utopia must be preserved at all costs.", choice1Destination: 9,
-            choice2: "Decline. This isn't the future I seek.", choice2Destination: 10
-        ),
-        Story(
-            title: "The whispers were of souls trapped in limbo, unable to board the ghost train. They're harmless and yearn for company.",
-            choice1: "Stay and keep them company. They need me.", choice1Destination: 0,
-            choice2: "Find a way to free them. Everyone deserves peace.", choice2Destination: 11
-        ),
-        Story(
-            title: "You avert the event, but as you return to the present, you realize your life has changed. New memories flood in. Is this better?",
-            choice1: "This is my new reality. I must live it.", choice1Destination: 0,
-            choice2: "Try to return and undo this change.", choice2Destination: 2
-        ),
-        Story(
-            title: "You watch as history unfolds. The event occurs, but with new insight, you understand its necessity in your life's tapestry.",
-            choice1: "Embrace the life lessons learned.", choice1Destination: 0,
-            choice2: "Go back and choose 'Tomorrow' instead.", choice2Destination: 5
-        ),
-        Story(
-            title: "Utopia is kept afloat by your actions. But with each task, you feel a piece of yourself fade. Is this the cost of perfection?",
-            choice1: "Endure. The greater good requires sacrifice.", choice1Destination: 0,
-            choice2: "Flee. Find a new future where I can be true to myself.", choice2Destination: 10
-        ),
-        Story(
-            title: "You reject the offer and the utopia crumbles. The reality behind the facade is grim. People are now free but at what cost?",
-            choice1: "Help rebuild a more honest society.", choice1Destination: 0,
-            choice2: "Leave. This is not the place for me.", choice2Destination: 2
-        ),
-        Story(
-            title: "Using an ancient ritual, you release the souls. The station brightens, and a true ghost train arrives to thank you. It offers a ride to any time or place.",
-            choice1: "I’ve done my part. Take me home.", choice1Destination: 0,
-            choice2: "I'm ready for another adventure. Surprise me.", choice2Destination: 2
+            title: "Safe and sound, you wait for your ride at the ranger's cabin, grateful for the kindness of strangers.",
+            choice1: "End", choice1Destination: 0,
+            choice2: "End", choice2Destination: 0
         )
+
     ]
 
     var storyIndex = 0 // Index of current story
--- MyAdventure/ViewController.swift
+++ MyAdventure/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyContent = StoryContent()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+    
+
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyContent.nextStory(sender.currentTitle!)
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyContent.getTitle()
+        choice1Button.setTitle(storyContent.getChoice1(), for: UIControl.State.normal)
+        choice2Button.setTitle(storyContent.getChoice2(), for: UIControl.State.normal)
+    }
+    
 }
-
